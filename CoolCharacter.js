99f6f448520003a09992f80b1924c0bd39bdac16
Guest User <Guest@Thes-MacBook-Pro.local>
2017-12-30 18:18:52 -0800

    at barnes

--- CoolCharacter.js
+++ CoolCharacter.js
@@ -5,29 +5,41 @@ var Character = function(name,life,atk,def) {
 	this.life = life;
 	this.atk = atk;
 	this.def = def;
-	console.log();
+	
+	this.displaystats = function(){
+
+	console.log(char1);
+	console.log(char2);
+	console.log(char3);
+	}
 	// this.displaystats = function() {
 	// var createChar = new Character(this.name,this.life,this.atk,this.def);
-
-	// }
 }
+
+
 
 var char1 = new Character("Timmy", 200, 7, 5);
 var char2 = new Character ("Bob", 300, 10, 3);
 var char3 = new Character ("Yankee Noodle", 100, 15, 0);
 
+character.displaystats();
 
-console.log(char1);
-console.log(char2);
 
-//create attack function 
+//create attack functions 
 function attackFromChar1() {
-	console.log(char2.life - (char1.atk - char2.def));
+	char2.life - (char1.atk - char2.def);
+
 
 	//(char2.life - (char1.atk - char2.def));
 }
 
-attackFromChar1();
+function attackFromChar2() {
+
+
+
+}
+
+
 
 
 
