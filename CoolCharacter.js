bf3cc7b5fa83448310e23ebb723bb38e8c5b57eb
Guest User <Guest@Thes-MBP.attlocal.net>
2017-12-31 19:42:37 -0800

    updates

--- CoolCharacter.js
+++ CoolCharacter.js
@@ -1,7 +1,117 @@
 var inquirer = require('inquirer');
+var fs = require('fs')
 
-var Character = function(name,life,atk,def) {
+
+function StartGame(){
+inquirer
+  .prompt([
+    {
+      type: 'checkbox',
+      message: 'Make your selections',
+      name: 'selections',
+      choices: [
+        {
+          name: "Character Create"
+        },
+        { 
+          name: "Ready for battle!"
+        }
+        ],
+    }
+    ])
+    .then(answers => {
+      //console.log(answers.selections)
+      if (answers.selections == "Character Create"){
+
+        //====create character function
+        inquirer
+          .prompt([
+            {
+              type: 'input',
+              message: 'Type a name!',
+              name: 'name',
+            },
+            {
+              type: 'input',
+              message: 'Type your Type!',
+              name: 'type',
+            },
+            {
+              type: 'input',
+              message: 'Type your HP! ',
+              name: 'hp',
+            },
+            {
+              type: 'input',
+              message: 'Type your Attack Points! ',
+              name: 'atk',
+            },
+            {
+              type: 'input',
+              message: 'Type your Defense Points! ',
+              name: 'def',
+            }
+            ])
+          
+
+            .then(answers => {
+              var playerInfo = ("\n==============\nName: "+answers.name + 
+                         "\n" + "Type: " + answers.type + 
+                         "\n" + "HP: " + answers.hp + 
+                         "\n" + "Atk Pts: "+answers.atk + 
+                         "\n" + "Def Pts: " + answers.def + "\n==============\n");
+              checkStat();
+              function checkStat(){
+                 if (answers.hp > 100 || answers.atk > 50 || answers.def > 100) {
+                  
+                  console.log(" HP cannot exceed 100 pts\n ATTACK cannot exceed 50 pts\n DEFENSE cannot exceed 100 pts!");
+                   StartGame();
+                 } else {
+                    fs.appendFile("playerInfo.txt", playerInfo, function (err) {
+                      console.log("info saved!");  
+                    }); 
+                 }
+              }
+                   
+            });
+          
+        //========
+        
+      } else if (answers.selections == "Ready for battle!"){
+        console.log("Game on!");
+
+        inquirer
+  .prompt([
+    {
+      type: 'checkbox',
+      message: 'Make your selections',
+      name: 'selections',
+      choices: [
+        {
+          name: "Attack Enemy!"
+        },
+         {
+          name: "Defense Mode!"
+        },
+         {
+          name: "Heal!"
+        },
+        { 
+          name: "Dodge!"
+        }
+        ],
+    }
+    ])
+        
+      }
+    });
+ }
+
+ StartGame();
+
+var Character = function(name,type,life,atk,def) {
 	this.name = name; 
+  this.type = type
 	this.life = life;
 	this.atk = atk;
 	this.def = def;
@@ -9,8 +119,6 @@ var Character = function(name,life,atk,def) {
 	this.displaystats = function(){
 
 	console.log(char1);
-	console.log(char2);
-	console.log(char3);
 	}
 	// this.displaystats = function() {
 	// var createChar = new Character(this.name,this.life,this.atk,this.def);
@@ -18,30 +126,20 @@ var Character = function(name,life,atk,def) {
 
 
 
-var char1 = new Character("Timmy", 200, 7, 5);
-var char2 = new Character ("Bob", 300, 10, 3);
-var char3 = new Character ("Yankee Noodle", 100, 15, 0);
+var char1 = new Character(this.inputName, this.inputType, this.inputPower, this.inputAtk, this.inputDef);
+// var char2 = new Character ("Bob", Fire, 300, 10, 3);
+// var char3 = new Character ("Yankee Noodle", Annoying Ray, 100, 15, 0);
 
-character.displaystats();
+//char1.displaystats();
 
 
 //create attack functions 
-function attackFromChar1() {
+function attackChar() {
 	char2.life - (char1.atk - char2.def);
-
-
 	//(char2.life - (char1.atk - char2.def));
 }
 
 function attackFromChar2() {
 
-
-
 }
-
-
-
-
-
-
 
--- algo2.js
+++ algo2.js
@@ -1,19 +1,76 @@
+// numArr = [];
+function convertToRoman(num) {
+num.toString();
+// console.log(num);
+var numString = num.toString(); 
+console.log(numString);
+  // numArr.push(num);
+  // //console.log(numArr);
+  // var divByTen = num / 10; console.log("divByTen",divByTen);
+  // var remOfTen = num % 10; console.log("remOfTen",remOfTen);
+  // var divByFive = num / 5; console.log("divByFive",divByFive);
+  // var remOfFive = num % 5; console.log("remOfFive",remOfFive);
 
-function diffArray(a1, a2) {
-  var result = [];
-    for (var i = 0; i < a1.length; i++) {
-      if (a2.indexOf(a1[i]) === -1) {
-        result.push(a1[i]);
-      }
-    }
-    for (i = 0; i < a2.length; i++) {
-      if (a1.indexOf(a2[i]) === -1) {
-        result.push(a2[i]);
-      }
-    }
-    console.log(result);
-    return result;
-}
+  // if((num < 100) && (remOfTen === 0) && (divByTen === 1)) {
+  //   numArr.splice(0,1,"X");
+  //   console.log(numArr.join(' '));
+  // }
+  // if((num < 100) && (remOfTen === 0) && (divByTen === 2)) {
+  //   numArr.splice(0,1,"XX");
+  //   console.log(numArr.join(' '));
+  // }
+  // if((num < 100) && (remOfTen === 0) && (divByTen === 3)) {
+  //   numArr.splice(0,1,"XXX");
+  //   console.log(numArr.join(' '));
+  // }
+  // if((num < 100) && (remOfFive === 0) && (divByFive === 10)) {
+  //   numArr.splice(0,1,"V");
+  //   console.log(numArr.join(' '));
+  // }
+  // if((num < 100) && (remOfFive === 0) && (divByFive === 10)) {
+  //   numArr.splice(0,1,"V");
+  //   console.log(numArr.join(' '));
+  }
 
-diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+  
+  
+  // console.log(divByTen, remOfTen, divByFive, remOfFive);
+
+
+  // if (remOfTen === 0) {
+  //   numArr.push(divByTen); //console.log(numArr);
+  //   if(numArr.indexOf(3) > -1){
+  //     replace = numArr.splice(numArr.indexOf(3),1,"XXX");
+  //     console.log(replace)
+  //   }
+  // }
+
+// }
+
+// 36 = XXXVI
+
+convertToRoman(50);
+
+// //===
+// function diffArray(a1, a2) {
+//   //Array for global inside this function
+//   var result = [];
+
+//     for (var i = 0; i < a1.length; i++) {
+//       if (a2.indexOf(a1[i]) === -1) {
+//         result.push(a1[i]); 
+//       }
+//       console.log(a2.indexOf(a1[i]));
+//     }
+//     for (i = 0; i < a2.length; i++) {
+//       if (a1.indexOf(a2[i]) === -1) {
+//         result.push(a2[i]);
+//       }
+//       console.log(result);
+//     }
+//     console.log(result);
+//     return result;
+// }
+
+// diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
 
